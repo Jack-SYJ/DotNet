51beb943bb248d32e42f178c619a67a94fbd2dc2
jamessu <jamessu@tencent.com>
2021-11-11 10:40:02 +0800

    waiting complete

--- Jakc.Extend.MySQL/MySqlBatcher.cs
+++ Jakc.Extend.MySQL/MySqlBatcher.cs
@@ -2,59 +2,34 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
+using System.Linq;
 using System.Text;
-using MySql.Data.MySqlClient;
 
 namespace Jack.Extend.MySQL
 {
     /// <summary>
-    /// 为 MySql.Data 组件提供的用于批量操作的方法。
     /// 参考 cnblogs.com/luluping/archive/2012/08/09/2629515.html
     /// </summary>
-    public sealed class MySqlBatcher 
+    public sealed class MySqlBatcher
     {
-
-        /// <summary>
-        /// 将 <see cref="DataTable"/> 的数据批量插入到数据库中。
-        /// </summary>
-        /// <param name="dataTable">要批量插入的 <see cref="DataTable"/>。</param>
-        public bool BulkInsert(DataTable dataTable,MySqlConnection connection,MySqlTransaction transaction=null)
-        {
-            if (dataTable.Rows.Count == 0)
-            {
-                return false;
-            }
-            if (connection == null)
-            {
-                return false;
-            }
-
-            if (connection.State != ConnectionState.Open)
-            {
-                connection.Open();
-            } 
-            using (var command = new MySqlCommand())
-            {
-                command.Connection = connection;
-                command.CommandText = GenerateInserSql(command, dataTable);
-                command.Transaction = transaction;
-                if (command.CommandText == string.Empty)
-                {
-                    return false;
-                }
-                return command.ExecuteNonQuery()>0;
-            }
-        }
 
         /// <summary>
         /// 生成插入数据的sql语句。
         /// </summary>
-        /// <param name="database"></param>
+        /// <typeparam name="T"></typeparam>
         /// <param name="command"></param>
         /// <param name="table"></param>
-        /// <returns></returns>
-        private string GenerateInserSql( DbCommand command, DataTable table)
+        /// <param name="dbParameter"></param>
+        public void GenerateInserSql<T>(IDbCommand command, DataTable table,T dbParameter) where T:DbParameter
         {
+            if (table==null || table.Rows.Count == 0)
+            {
+                throw new ArgumentNullException("DataTable中无数据");
+            }
+            if (dbParameter == null)
+            {
+                throw new ArgumentNullException("dbParameter must be not null");
+            }
             var names = new StringBuilder();
             var values = new StringBuilder();
             var types = new List<DbType>();
@@ -85,7 +60,7 @@ namespace Jack.Extend.MySQL
                         values.Append(", ");
                     }
                     var isStrType = IsStringType(types[j]);
-                    var parameter = CreateParameter(isStrType, types[j], row[j], i, j);
+                    var parameter = CreateParameter(isStrType, types[j], row[j], i, j, dbParameter);
                     if (parameter != null)
                     {
                         values.Append(parameter.ParameterName);
@@ -103,8 +78,81 @@ namespace Jack.Extend.MySQL
                 values.Append(")");
                 i++;
             }
-            return string.Format("INSERT INTO {0}({1}) VALUES {2}", table.TableName, names, values);
+            command.CommandText= string.Format("INSERT INTO {0}({1}) VALUES {2}", table.TableName, names, values);
         }
+
+        /// <summary>
+        /// 生成插入数据的sql语句。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="command"></param>
+        /// <param name="table"></param>
+        /// <param name="dbParameter"></param>
+        public Tuple<string, List<DbParameter>> GenerateInserSql<T, P>(IEnumerable<T> datas, P dbParameter) where T : class where P : DbParameter
+        {
+            if (datas == null || datas.Count() == 0)
+            {
+                throw new ArgumentNullException("datas must be has value");
+            }
+            if (dbParameter == null)
+            {
+                throw new ArgumentNullException("dbParameter must be not null");
+            }
+            var names = new StringBuilder();
+            var values = new StringBuilder();
+            var types = new List<DbType>();
+            List<P> parameters = new List<P>();
+
+            Type typeT = typeof(T);
+
+            var count = table.Columns.Count;
+
+            for (int j = 0; j < count; j++)
+            {
+                if (names.Length > 0)
+                {
+                    names.Append(",");
+                }
+                names.AppendFormat("{0}", table.Columns[j].ColumnName);
+                types.Add((DbType)Enum.Parse(typeof(DbType), table.Columns[j].DataType.Name));
+            }
+
+            var i = 0;
+            foreach (DataRow row in table.Rows)
+            {
+                if (i > 0)
+                {
+                    values.Append(",");
+                }
+                values.Append("(");
+                for (var j = 0; j < count; j++)
+                {
+                    if (j > 0)
+                    {
+                        values.Append(", ");
+                    }
+                    var isStrType = IsStringType(types[j]);
+                    var parameter = CreateParameter(isStrType, types[j], row[j], i, j, dbParameter);
+                    if (parameter != null)
+                    {
+                        values.Append(parameter.ParameterName);
+                        parameters.Add(parameter);
+                    }
+                    else if (isStrType)
+                    {
+                        values.AppendFormat("'{0}'", row[j]);
+                    }
+                    else
+                    {
+                        values.Append(row[j]);
+                    }
+                }
+                values.Append(")");
+                i++;
+            }
+            command.CommandText = string.Format("INSERT INTO {0}({1}) VALUES {2}", table.TableName, names, values);
+        }
+
 
         /// <summary>
         /// 判断是否为字符串类别。
@@ -125,20 +173,22 @@ namespace Jack.Extend.MySQL
         /// <param name="row"></param>
         /// <param name="col"></param>
         /// <returns></returns>
-        private DbParameter CreateParameter(bool isStrType, DbType dbType, object value, int row, int col)
+        private T CreateParameter<T>(bool isStrType, DbType dbType, object value, int row, int col,T t) where T:DbParameter
         {
             //如果生成全部的参数，则速度会很慢，因此，只有数据类型为字符串(包含'号)和日期型时才添加参数
             if ((isStrType && value.ToString().IndexOf('\'') != -1) || dbType == DbType.DateTime)
             {
                 var name = string.Format("@p_{0}_{1}", row, col);
-                var parameter = new MySqlParameter();
+                var parameter = Activator.CreateInstance<T>();
                 parameter.ParameterName = name;
                 parameter.Direction = ParameterDirection.Input;
                 parameter.DbType = dbType;
                 parameter.Value = value;
-                return parameter;
+                return parameter ;
             }
             return null;
         }
     }
+
+   
 }
--- Jakc.Test/Program.cs
+++ Jakc.Test/Program.cs
@@ -2,12 +2,52 @@
 using System.Collections.Generic;
 using Jack.Extend.MySQL;
 using System.Threading;
+using System.Data;
+using System.Collections;
+using MySql.Data.MySqlClient;
+
 namespace Jakc.Test
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            var datatable = new DataTable();
+            datatable.TableName = "AppAuth";
+            datatable.Columns.Add(new DataColumn
+            {
+                ColumnName = "accessId",
+                DataType = typeof(Int32),
+            });
+            datatable.Columns.Add(new DataColumn
+            {
+                ColumnName = "appId",
+                DataType = typeof(string),
+                MaxLength = 50
+            });
+            datatable.Columns.Add(new DataColumn
+            {
+                ColumnName = "accessName",
+                DataType = typeof(string),
+                MaxLength = 200
+            });
+            datatable.Columns.Add(new DataColumn
+            {
+                ColumnName = "tenancyId",
+                DataType = typeof(string),
+                MaxLength = 50
+            });
+
+            for (int i = 0; i < 10; i++)
+            {
+                ArrayList array = new ArrayList();
+                array.Add(0);
+                array.Add($"id_{i}");
+                array.Add($"name_{i}");
+                array.Add($"name#@_''\"ten_{i}");
+                datatable.LoadDataRow(array.ToArray(), true);
+            }
 
             List<Table1> table1s = new List<Table1>();
             for (int i = 0; i < 3; i++)
@@ -21,10 +61,45 @@ namespace Jakc.Test
                 });
                 Thread.Sleep(1000);
             }
+            var batch = new MySqlBatcher();
 
-            var result = table1s.CreateBulkSql();
+            // var table = new DataTable();
 
-            Console.WriteLine("Hello World!");
+            using (var connection = new MySqlConnection("server=9.134.119.192;userid=root;pwd=Tencent@2020;port=3306;database=test2;sslmode=none;"))
+            {
+                connection.Open();
+                using (var command = new MySqlCommand())
+                {
+                    if (command == null)
+                    {
+                        throw new ArgumentException("command");
+                    }
+                    command.Connection = connection;
+
+
+                    command.Transaction = null;
+
+
+                    var para = new MySqlParameter();
+                    batch.(datatable, command,para );
+
+
+                    if (command.CommandText == string.Empty)
+                    {
+                        return;
+                    }
+
+
+                    command.ExecuteNonQuery();
+                }
+
+
+
+
+                //var result = table1s.CreateBulkSql();
+
+                Console.WriteLine("Hello World!");
+            }
         }
     }
 }
