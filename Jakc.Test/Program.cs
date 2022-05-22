fa38b40c6d041efb195e40d77c2e880b5a8c160b
jamessu <jamessu@tencent.com>
2022-05-22 11:29:05 +0800

    新增测试

--- Jakc.Test/Program.cs
+++ Jakc.Test/Program.cs
@@ -91,41 +91,46 @@ namespace Jakc.Test
         }
         static void IEnumerableTest()
         {
-
-            List<AppAuth> appAuths=new List<AppAuth>();
-            for (int i = 0; i < 20; i++)
+             DateTime dateTime = DateTime.Now;  
+            for (int i = 0; i < 200; i++)
             {
-                appAuths.Add(new AppAuth
+                List<AppAuth> appAuths = new List<AppAuth>();
+
+                for (int j = 0; j < 5000; j++)
                 {
-                    appId = i + 154,
-                    accessName = "accessName&*^%*^*&^%%*)(*",
-                    tenancyId = "tencent''\"\"",
-                    CreateTime=DateTime.Now,
-                    UpdateTime=(i%2==0)?DateTime.Now:null
-                }); 
-            }
-
-            var batch = new MySqlBatcher();
-
-            using (var connection = new MySqlConnection("server=9.134.119.192;userid=root;pwd=Tencent@2020;port=3306;database=test2;sslmode=none;"))
-            {
-                connection.Open();
-                int count = 0;
-                using (var command = connection.CreateCommand())
-                {
-                   var result= batch.GenerateInserSql<AppAuth,MySqlParameter>(appAuths);
-
-                    command.CommandText = result.Item1;
-                    if (result.Item2.Count > 0)
+                    appAuths.Add(new AppAuth
                     {
-                        command.Parameters.AddRange(result.Item2.ToArray());
-                    }
-
-                    count = command.ExecuteNonQuery();
+                        appId = i*j + 154,
+                        accessName = "accessName&*^%*^*&^%%*活动哈哦啊接哦我解耦我agoiaoigoiojoidjoiewj地奇偶决定我奥IjoiajoijoiJOIfjoiejoigjoidjaoij)(*",
+                        tenancyId = "tencent''\"\"",
+                        CreateTime = DateTime.Now,
+                        UpdateTime = (j % 7 == 0) ? DateTime.Now : null
+                    });
                 }
 
-                Console.WriteLine($" insert count [{count}]");
+                var batch = new MySqlBatcher();
+
+                using (var connection = new MySqlConnection("server=127.0.0.1;userid=root;pwd=1000@TbTdTgT;port=3306;database=test2;sslmode=none;"))
+                {
+                    connection.Open();
+                    int count = 0;
+                    using (var command = connection.CreateCommand())
+                    {
+                        var result = batch.GenerateInserSql<AppAuth, MySqlParameter>(appAuths);
+
+                        command.CommandText = result.Item1;
+                        if (result.Item2.Count > 0)
+                        {
+                            command.Parameters.AddRange(result.Item2.ToArray());
+                        }
+
+                        count = command.ExecuteNonQuery();
+                    }
+
+                    Console.WriteLine($"{(DateTime.Now-dateTime).TotalSeconds}秒，循环值：[{i}] insert count [{count}]");
+                }
             }
+            
         }
 
     }
